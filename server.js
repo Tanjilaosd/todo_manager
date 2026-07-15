28d818ab97af1f48dbb00ad6934484f0fe719ce2
tanjilaakter123 <Tanjilajumur24@gmail.com>
2026-07-15 09:03:50 +0600

    second commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT
 
 import app from "./src/app.js";
 
-const PORT = port || 3000;
+const PORT = port || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
