c7c92c8ece17eda4d553810350b436fb011d9ca1
tanjilaakter123 <Tanjilajumur24@gmail.com>
2026-07-16 22:16:42 +0600

    auth models created

---
+++ src/modules/auth/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+import { validation } from '../../../shared/constant'
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,"name is required"],
+        trim: true,
+        maxLength : [validation.nameMaxLength,`name cannot exceed ${validation.nameMaxLength} characters`]
+
+    },
+    email : {
+        type : String,
+        required: [true,'email is required'],
+        trim: true,
+        unique : true,
+        lowercase : true,
+        match:[/^\s+@\s+\.\s+$/,"please provide a valid email"],
+
+    },
+    password:{
+        type: String,
+        required: [true,"password is required"],
+        minLength: [validation.passwordMinLength,`password must be at least ${validation.passwordMinLength} characteres`],
+        select: false
+    }
+},{
+    timestamps: true,
+    versionKey:false
+})
+
+export const User = mongoose.models.User || mongoose.model("User",userSchema)
---
+++ src/shared/constant.js
@@ -0,0 +1,4 @@
+export const validation = Object.freeze({
+    nameMaxLength : 50,
+    passwordMinLength : 6
+})
